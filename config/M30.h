562164c8f78d881c501ec3f6159e785625ab9f56
kenorb <kenorb@users.noreply.github.com>
2021-08-09 21:20:30 +0100

    Renames signal open filter param as per the latest classes change

--- config/M30.h
+++ config/M30.h
@@ -9,7 +9,6 @@ struct Stg_TMA_Band_SVE_True_EURUSD_M30_Params : Stg_TMA_Band_SVE_True_Params {
     TMA_Band_SVE_True_LotSize = lot_size = 0;
     TMA_Band_SVE_True_Shift = 0;
     TMA_Band_SVE_True_SignalOpenMethod = signal_open_method = 2;
-    TMA_Band_SVE_True_SignalOpenFilterMethod = signal_open_filter = 32;
     TMA_Band_SVE_True_SignalOpenLevel = signal_open_level = (float)0;
     TMA_Band_SVE_True_SignalOpenBoostMethod = signal_open_boost = 0;
     TMA_Band_SVE_True_SignalCloseMethod = signal_close_method = 2;
